c273e5fae77994002e5ac6e5bf902db8c3f1db10
hugh672 <hugh672@hotmail.co.uk>
2018-04-19 10:59:47 +0100

    Added Multiplier

---
+++ src/test/java/com/qa/calculator/MultiplierTest.java
@@ -0,0 +1,20 @@
+package com.qa.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MultiplierTest {
+
+	private Multiplier multiplier;
+	
+	@Test
+	public void test() {
+		multiplier = new Multiplier();
+		int expectedValue = 10;
+		int actualValue = multiplier.multiply(5,2);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+
+}
